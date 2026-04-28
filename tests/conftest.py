5b2027b720a6b11e93a6a1d2821f629e2b7f342a
Oleksandr <93466475+oleksandrmyhydiuk@users.noreply.github.com>
2026-04-28 19:23:14 +0300

    fix: remove webdriver-manager and use native Selenium Manager

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function")
@@ -14,8 +12,7 @@ def driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
 
     yield driver
 
